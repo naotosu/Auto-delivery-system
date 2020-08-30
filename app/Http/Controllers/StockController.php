44f60811081909bcc233b5fbdf0ab97f563289ff
naotosu <re13brb26l28@hotmail.com>
2020-08-31 06:44:57 +0900

    7、トップページ実装-2

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -4,25 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class stockController extends Controller
+class StockController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    /*public function __construct()
-    {
-        $this->middleware('auth');
-    }*/
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-
-
     public function incoming()
     {
         return view('incoming');
