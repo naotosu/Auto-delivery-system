53b3d1380c1511e17ee95187cec045c11cf993d2
naotosu <re13brb26l28@hotmail.com>
2020-09-04 00:32:01 +0900

    ロードマップ8.5出荷予定一覧

--- laravel/app/Http/Controllers/StockController.php
+++ laravel/app/Http/Controllers/StockController.php
@@ -19,6 +19,19 @@ class StockController extends Controller
         return view('order', $data);
     }
 
+    public function ordersindex(Request $request)
+    {
+        $item_id = $request->input('item_id');
+        $order_start = $request->input('order_start');
+        // $order_end = $request->input('order_end'); 
+
+        $item_id = order::where('item_id','like','%'.$item_id.'%')->first();
+        $order_start = order::where('date','like','%'.$order_start.'%')->first();
+        // $order_end = order::where('date','like','%'.$order_end.'%')
+
+        return view('ordersindex', ['item_ids'=>$item_id,'order_starts'=>$order_start]);
+    }
+
     public function compulsion()
     {
         return view('compulsion');
@@ -29,7 +42,10 @@ class StockController extends Controller
         return view('extraordinary');
     }
 
-
+    public function stock()
+    {
+        return view('stock');
+    }
 
 
 
