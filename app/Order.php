765fb7273fda4f5329276a1287e7cf19e0156be6
naotosu <re13brb26l28@hotmail.com>
2020-09-04 16:34:57 +0900

    ロードマップ8.5出荷予定一覧

--- app/Order.php
+++ app/Order.php
@@ -9,4 +9,15 @@ class Order extends Model
     protected $table = 'orders';
     protected $primaryKey = 'id';
     public $timestamps = false;
+
+    public function orderIndex(Builder $order_index, array $params): Builder
+    {
+    	//$order_index = order::query();　←このページでは不要？
+    	$order_index
+    		->where('item_id', $params['item_id'])
+    		->andwhereColumn('order_start', '<' 'order_end');
+    	$order_indexs = $order_index->get();
+
+    	return $order_indexs;
+    }
 }
--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -14,14 +14,14 @@
     <div class="main2">
         <h3>出荷予定</h3>
 
-        <form action="{{url('/ordersindex')}}" method="GET">
-            <p><label for="item_id">アイテムコードを選んで下さい。
-                <input type="text" name="item_id" value="">
+        <form action="{{url('/orders')}}" method="GET">
+            <p><label for="item_id">アイテムコードを入力して下さい。
+                <input type="text" name="item_id" value="{{ $params['item_id'] ?? null }}">
             </label></p>
 
             <p><label for="order_date">表示する納入日を選んで下さい。
-                    <input type="date" name="order_start" min="2020-08-01" max="2020-10-01">〜
-                    <!-- <input type="date" name="order_end" min="2020-08-01" max="2020-10-01"> -->
+                    <input type="date" name="order_start" min="2020-08-01" max="2020-10-01" value="{{ $params['order_start'] ?? null }}">〜
+                    <input type="date" name="order_end" min="2020-08-01" max="2020-10-01" value="{{ $params['order_end'] ?? null }}">
             </label></p>
 
             <p><input type="submit" value="検索"></p>
@@ -36,11 +36,11 @@
                 <th>納入日</th>
                 <th>数量</th>
             </tr>
-            @foreach ($orders as $order)
+            @foreach ($order_indexs as $order)
             <tr>
-                <td><a href="{{ url('/ordersindex') }}/?$order->item_id }}" >{{$order->item_id}}</a></td>
-                <td><a href="{{ url('/ordersindex') }}/?$order->delivery_date }}" >{{$order->delivery_date}}</a></td>
-                <td><a href="{{ url('/ordersindex') }}/?$order->quantity }}" >{{$order->quantity}}</a></td>
+                <td>{{$order->item_id}}</td>
+                <td>{{$delivery_date}}</td>
+                <td>{{$order->quantity}}</td>
             </tr>
             @endforeach
         </table>
