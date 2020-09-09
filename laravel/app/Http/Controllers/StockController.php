512b84a288942c357374bd439a0a4b65260a2e7f
naotosu <re13brb26l28@hotmail.com>
2020-09-09 15:19:26 +0900

    ロードマップ8.5テーブル＆シーダー追加

--- laravel/app/Http/Controllers/StockController.php
+++ laravel/app/Http/Controllers/StockController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Order;
-use App\Item;
 use Carbon\Carbon;
 
 class StockController extends Controller
--- laravel/app/Order.php
+++ laravel/app/Order.php
@@ -12,45 +12,19 @@ class Order extends Model
 
     public function scopeOrderIndex($query, $item_id, $delivery_user_id, $order_start, $order_end)
     {
-      	if (!empty($item_id) and !empty($delivery_user_id) and !empty($order_start) and !empty($order_end)) {
+      	if (isset($item_id)) {
+		    $query->where('item_id', $item_id);
+		}
 
-	      	return $query
-	    		->where('item_id', $item_id)
-	    		->where('delivery_user_id', $delivery_user_id)
-	    		->whereBetween('orders.delivery_date', [$order_start, $order_end]);
+		if (isset($delivery_user_id)) {
+		    $query->where('delivery_user_id', $delivery_user_id);
+		}
 
-	    } elseif (!empty($item_id) and !empty($delivery_user_id)) {
+		if (isset($order_start) and isset($order_end)) {
+			$query->whereBetween('delivery_date', [$order_start, $order_end]);
+		}
 
-	      	return $query
-	    		->where('item_id', $item_id)
-	    		->where('delivery_user_id', $delivery_user_id);
-
-	    } elseif (!empty($delivery_user_id) and !empty($order_start) and !empty($order_end)) {
-
-	      	return $query
-	    		->where('delivery_user_id', $delivery_user_id)
-	    		->whereBetween('orders.delivery_date', [$order_start, $order_end]);
-
-	    } elseif (!empty($item_id) and !empty($order_start) and !empty($order_end)) {
-
-	      	return $query
-	    		->where('item_id', $item_id)
-	    		->whereBetween('orders.delivery_date', [$order_start, $order_end]);
-
-	    } elseif (!empty($item_id)){
-
-	   		 return $query
-	   		 	->where('orders.item_id', $item_id);
-
-	   	} elseif (!empty($delivery_user_id)){
-
-	   		 return $query
-	   		 	->where('delivery_user_id', $delivery_user_id);
-	    		
-	    } else {
-	    	return $query
-	    		->whereBetween('orders.delivery_date', [$order_start, $order_end]);
-	    }
+		return $query;
     }
 
     public function item()
