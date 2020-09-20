35998d260af6108198f1bceae391f7cd37784901
naotosu <re13brb26l28@hotmail.com>
2020-09-20 12:08:47 +0900

    ロードマップ12API化

--- app/Http/Controllers/API/CsvController.php
+++ app/Http/Controllers/API/CsvController.php
@@ -20,61 +20,7 @@ class CsvController extends Controller
         //$change_id = $request->input('change_id');
         //$item_ids = [$request->input('item_ids')];
 
-        //return　TemporaryService::TemporaryIndex();
-        return response()->streamDownload(
-            function () {
-                // 出力バッファをopen
-                $stream = fopen('php://output', 'w');
-                // 文字コードをShift-JISに変換
-                stream_filter_prepend($stream,'convert.iconv.utf-8/cp932//TRANSLIT');
-                // ヘッダー
-                fputcsv($stream, [
-                    '配送業者ID',
-                    '配送業者',
-                    '担当',
-                    '納品先コード',
-                    '納品先',
-                    'オーダーNo',
-                    '鋼種',
-                    'サイズ',
-                    '単位',
-                    '仕様',
-                    '納入日',
-                    '製造No',
-                    '結番',
-                    '重量',
-                    '本数',
-                ]);
-
-                // データ
-                $temporary_ships = Inventory::TemporaryShip()->get();
-
-                foreach ($temporary_ships as $temporary){
-                    fputcsv($stream, [
-                        $temporary->order->transport_id,
-                        $temporary->order->transportCompany->name,
-                        $temporary->order->transportCompany->stuff_name,
-                        $temporary->order->delivery_user_id,
-                        $temporary->order->clientCompanyDeliveryUser->name, 
-                        $temporary->order_code,
-                        $temporary->item->name,
-                        $temporary->item->size, 
-                        $temporary->item->shape, 
-                        $temporary->item->spec,
-                        $temporary->ship_date,
-                        $temporary->manufacturing_code,
-                        $temporary->bundle_number,
-                        $temporary->weight,
-                        $temporary->quantity,
-                    ]);
-                }
-
-                fclose($stream);
-            },
-            'ship'.date('Y-m-d H:m:s').'.csv',
-            [
-                'Content-Type' => 'application/octet-stream',
-            ]
-        );
+        return TemporaryService::TemporaryIndex();
+        
     }
 }
