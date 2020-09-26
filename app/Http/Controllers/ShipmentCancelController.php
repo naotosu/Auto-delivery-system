2ddf366f3e11d68f66e50a6ac1a8ee8831ad2f76
naotosu <re13brb26l28@hotmail.com>
2020-09-26 21:17:34 +0900

    ロードマップ15出荷キャンセル　完成＆バグ修正

--- app/Http/Controllers/ShipmentCancelController.php
+++ app/Http/Controllers/ShipmentCancelController.php
@@ -27,22 +27,21 @@ class ShipmentCancelController extends Controller
 
     public function shipment_cancel_check(Request $request)
     {
-    	$item_code = $request->input('item_code');
+        $item_code = $request->input('item_code');
         $delivery_user_id = $request->input('delivery_user_id');
         $status = $request->input('status');
         $ship_date = $request->input('ship_date');
 
-        $item_ids = [$request->input('item_ids')];
-        $original_item_ids = $request->input('item_ids');
+        $item_ids = $request->input('item_ids');
         $status_edit = $request->input('status_edit');
 
         if (empty($status_edit)) {
         	$stock_indexes = Inventory::editIndex($item_code, $delivery_user_id, $status, $ship_date)->get();
             session()->flash('flash_message', 'どこまで進捗を戻すか？は入力必須です');
-            return view('cancel', compact('stock_indexes', 'item_code', 'delivery_user_id', 'status', 'ship_date', 'item_ids', 'original_item_ids'));
+            return view('cancel', compact('stock_indexes', 'item_code', 'delivery_user_id', 'status', 'ship_date', 'item_ids'));
         }
 
-        if (empty($original_item_ids)) {
+        if (empty($item_ids)) {
         	$stock_indexes = Inventory::editIndex($item_code, $delivery_user_id, $status, $ship_date)->get();
             session()->flash('flash_message', '出荷取消を行う対象を選択して下さい');
             return view('cancel', compact('stock_indexes', 'item_code', 'delivery_user_id', 'status', 'ship_date', 'status_edit'));
@@ -55,7 +54,7 @@ class ShipmentCancelController extends Controller
 
     public function shipment_cancel_execute(Request $request)
     {
-        $item_ids = [$request->input('item_ids')];
+        $item_ids = $request->input('item_ids');
         $status_edit = $request->input('status_edit');
 
         try {   
