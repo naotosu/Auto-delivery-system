b4ca97c9f24aa3ff9d1e4e958538fb20e99d5da4
naotosu <re13brb26l28@hotmail.com>
2020-09-28 22:55:56 +0900

    ロードマップ16spreadsheets-export-test array_push対策途中

--- app/Console/Commands/AutoDeliveryCommand.php
+++ app/Console/Commands/AutoDeliveryCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\OrderItem;
+use App\Models\Inventory;
 use App\Models\GoogleSheet;
 use Carbon\Carbon;
 
@@ -42,18 +43,20 @@ class AutoDeliveryCommand extends Command
     {
         $sheets = GoogleSheet::OrderItem();
 
-        $sheet_id = '1DRe3JKouPvmXoosZXlhXcNOGnALHO61J39QTItwAMHc';
-        $ship_date = '2020-09-29';
-        $order_indexes = OrderItem::AutoDeliveryIndex($ship_date)->get();
+        //$service = new \Google_Service_Sheets($client);
 
-        $shipment_lists = Inventory::ShipmentList($order_indexes)->post();
+
+        $sheet_id = '1DRe3JKouPvmXoosZXlhXcNOGnALHO61J39QTItwAMHc';
+        $valueInputOption = "USER_ENTERED";
+        $ship_date = '2020-09-30';
+        $order_indexes = OrderItem::AutoDeliveryIndex($ship_date)->get();
+        $range = 'B2:M200';
+
+        //$shipment_lists = Inventory::ShipmentList($order_indexes)->post();
 
         $order_items = array();
-        $x = 0;
      
         foreach ( $order_indexes as $order) {
-
-            //$order_items　=  [array_merge($order->order->transport_id)];
 
             array_push($order_items, [
                 $order->order->transport_id,
@@ -62,83 +65,26 @@ class AutoDeliveryCommand extends Command
                 $order->order->delivery_user_id,
                 $order->order->clientCompanyDeliveryUser->name
                 ]);
-
-            $x++;
         }
-
-        for ($i = 0; $i < $x; $i++ ) {
-            $values = new \Google_Service_Sheets_ValueRange();
-            $values->setValues([
-                'values' => $order_items[$i]
+        $data = new \Google_Service_Sheets_ValueRange();
+        $data->setValues([
+                'range' => $range, 
+                'values' => $order_items
             ]);
-            $params = ['valueInputOption' => 'USER_ENTERED'];
-            $sheets->spreadsheets_values->append(
+
+        $body = new \Google_Service_Sheets_BatchUpdateValuesRequest(array(
+            'valueInputOption' => $valueInputOption,
+            'data' => $data
+        ));
+        $result = $sheets->spreadsheets_values->batchUpdate($spreadsheetId, $body);
+
+
+            /*$params = ['valueInputOption' => 'USER_ENTERED'];
+            $sheets->spreadsheets_values->batchUpdate(
                 $sheet_id,
                 'A2',
                 $values,
                 $params
-            );
-
-        }
-
-  //          array_push($order_items, [$order->order->transport_id, $order->order->transportCompany->name]);
-            //array_push($order_items, $order->order->transportCompany->name);
-
-            /*array_push($order_items, $order->order->transport_id);
-            array_push($order_items, $order->order->transportCompany->name);
-            array_push($order_items, $order->order->transportCompany->stuff_name);
-            array_push($order_items, $order->order->delivery_user_id);
-            array_push($order_items, $order->order->clientCompanyDeliveryUser->name);
-            array_push($order_items, $order->order_code);
-            array_push($order_items, $order->item->name);
-
-            
-
-
-             /*           
-                        $temporary->,
-                        $temporary->, 
-                        $temporary->,
-                        $temporary->item->name,
-                        $temporary->item->size, 
-                        $temporary->item->shape, 
-                        $temporary->item->spec,
-                        $temporary->ship_date,
-                        $temporary->charge_code,
-                        $temporary->manufacturing_code,
-                        $temporary->bundle_number,
-                        $temporary->weight,
-                        $temporary->quantity,
-        
-                /*$order->item_code,
-                <td>{{$order->item->name}}</td>
-                <td>{{$order->ship_date}}</td>
-                <td>{{$order->quantity}}</td>
-                <td>{{$order->name}}（仮）</td>
-                <td>{{$order->name}}（仮）</td>
-                <td>{{$order->order->delivery_user_id}}</td>
-                <td>{{$order->order->clientCompanyDeliveryUser->name}}</td>
-                <td>{{$order->temporary_flag]*/
-        //}
-
-/*$test_array  = [
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f'],
-    ['g', 'h', 'i']
-];
-
-dd($test_array);
-
-        $values = new \Google_Service_Sheets_ValueRange();
-        $values->setValues([
-            'values' => $order_items
-        ]);
-        $params = ['valueInputOption' => 'USER_ENTERED'];
-        $sheets->spreadsheets_values->append(
-            $sheet_id,
-            'A2',
-            $values,
-            $params
-        );*/
+            );*/
     }
 }
