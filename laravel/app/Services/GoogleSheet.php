b252629f92bfe35060004f677025dcf34310f8c7
naotosu <re13brb26l28@hotmail.com>
2020-10-13 23:51:14 +0900

    メソッド名修正

--- laravel/app/Services/GoogleSheet.php
+++ laravel/app/Services/GoogleSheet.php
@@ -2,11 +2,9 @@
 
 namespace App\Services;
 
-use Illuminate\Database\Eloquent\Model;
-
-class GoogleSheet extends Model
+class GoogleSheet
 {
-    public static function OrderItem() {
+    public static function ClientSet() {
 
         $credentials_path = storage_path('app/json/credentials.json');
         $client = new \Google_Client();
