4d3c0ada4acf508e0253fec65311e85f32a2db4b
naotosu <re13brb26l28@hotmail.com>
2020-10-21 23:53:16 +0900

    error-log2

--- laravel/resources/views/common.blade.php
+++ laravel/resources/views/common.blade.php
@@ -4,7 +4,7 @@
     <title>{{ config('app.name') }}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/ads.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/ads.css') }}">
   </head>
 
   <body>
