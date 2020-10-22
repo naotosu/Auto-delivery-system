24150e86d45e03a0400625910d137606dbccea2a
naotosu <re13brb26l28@hotmail.com>
2020-10-23 00:06:26 +0900

    google_API認証3

--- laravel/app/Services/GoogleSheet.php
+++ laravel/app/Services/GoogleSheet.php
@@ -6,7 +6,7 @@ class GoogleSheet
 {
     public static function InitializeClient() {
 
-        $credentials_path = storage_path('app/json/credentials.json');
+        $credentials_path = storage_path('/app/google-credentials.json');
         $client = new \Google_Client();
         $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
         $client->setAuthConfig($credentials_path);
