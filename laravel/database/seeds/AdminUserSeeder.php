68287dd93d2728e561d711bd4816ba1f143a0c9c
naotosu <re13brb26l28@hotmail.com>
2020-10-23 23:03:37 +0900

    RFC対策

--- laravel/database/seeds/AdminUserSeeder.php
+++ laravel/database/seeds/AdminUserSeeder.php
@@ -14,7 +14,7 @@ class AdminUserSeeder extends Seeder
     {
     	User::create([
 		  'name' => 'guest',
-		  'email' => 'auto.delivery.system2020guest@gmail.com',
+		  'email' => 'guest@gmail.com',
 		  'password' => Hash::make('password'),
  		]);
     }
