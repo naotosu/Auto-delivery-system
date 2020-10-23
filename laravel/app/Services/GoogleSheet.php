bdb85db06332bf146561bdc098e24e9cb6fdbb48
naotosu <re13brb26l28@hotmail.com>
2020-10-24 00:28:16 +0900

    Log3

--- laravel/app/Services/GoogleSheet.php
+++ laravel/app/Services/GoogleSheet.php
@@ -1,15 +1,21 @@
 <?php
 
 namespace App\Services;
+use Illuminate\Support\Facades\Log;
 
 class GoogleSheet
 {
     public static function InitializeClient() {
 
+        Log::error('gg1');
         $credentials_path = storage_path('/app/google-credentials.json');
+        Log::error('gg2');
         $client = new \Google_Client();
+        Log::error('gg3');
         $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+        Log::error('gg4');
         $client->setAuthConfig($credentials_path);
+        Log::error('gg5');
         return new \Google_Service_Sheets($client);
     }
 }
